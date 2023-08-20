a2c8e3c4f7b89a9ebfa6176a1c5d43b18fe825c5
komlatha <garimidikomalatha62@gmail.com>
2023-08-20 22:22:24 +0530

    move all zeros in an to the end of the array

--- MoveZerosToEnd.java
+++ MoveZerosToEnd.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class MoveZerosToEnd {
+import java.util.Scanner;
+
+public class MoveZerosToEnd {
+    public static void zerosToEnd(int array[],int length){
+        int z=0;
+        int nz=0;
+        while(nz<length) {
+            if(array[nz]!=0){
+                int temp=array[z];
+                array[z]=array[nz];
+                array[nz]=temp;
+                nz++;
+                z++;
+            }
+            else{
+                nz++;
+            }
+
+            }
+
+        for(int k=0;k<array.length;k++){
+            System.out.print(array[k]+" ");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int length=scanner.nextInt();
+        int array[]=new int[length];
+        System.out.println("Enter the elements for array");
+        for(int i=0;i<length;i++){
+            array[i]= scanner.nextInt();
+        }
+
+        MoveZerosToEnd.zerosToEnd(array,length);
+    }
 }
