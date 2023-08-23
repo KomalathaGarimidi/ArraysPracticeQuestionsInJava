e08d57dabf188e42df50f75a089fa35def314042
komlatha <garimidikomalatha62@gmail.com>
2023-08-24 00:24:19 +0530

    check if a pair with given sum exits in array

---
+++ TwoSum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TwoSum {
+    public static String sum(int array[],int N,int target){
+        int sum;
+        boolean flag=false;
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<N;j++){
+                sum=0;
+                if(array[i]<target && array[j]<target){
+
+                    sum=array[i]+array[j];
+                }
+                if(sum==target){
+                    flag=true;
+                    break;
+                }
+
+            }
+            if(flag==true){
+                return "Yes";
+            }
+        }
+            return "No";
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int N= sc.nextInt();
+        int array[] =new int[N];
+        System.out.println("Accepting the values into an array");
+        for(int i=0;i<N;i++){
+            array[i]= sc.nextInt();
+        }
+        System.out.println("Enter the taarget value");
+        int target=sc.nextInt();
+        System.out.println(TwoSum.sum(array, N, target));
+    }
+}
